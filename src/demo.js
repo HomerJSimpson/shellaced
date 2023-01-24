3d33a6fb8df485c727d9c64bfcd49c9c4fda004d
Nicholas Lehman <nick.lehman@icims.com>
2023-01-24 15:51:58 -0500

    replace react-ipsum

--- src/demo.js
+++ src/demo.js
@@ -1,130 +1,174 @@
-import React from "react";
+import React from 'react';
 import {
-  Stack,
-  Divider,
-  Container,
-  IconButton,
-  Button,
-  List,
-  ListItem,
-  Drawer,
-  AppBar,
-  Toolbar,
-  Typography,
-  Box,
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+	Stack,
+	Divider,
+	Container,
+	IconButton,
+	Button,
+	List,
+	ListItem,
+	Drawer,
+	AppBar,
+	Toolbar,
+	Typography,
+	Box,
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 // import EmailIcon from "@mui/icons-material/Email";
-import { LoremText } from "react-ipsum";
-import { styled } from "@mui/material/styles";
+import { styled, useTheme } from '@mui/material/styles';
 
-import { PeopleIcon, WorkflowIcon, JobsIcon } from "./demo-icons";
+import { PeopleIcon, WorkflowIcon, JobsIcon } from './demo-icons';
 
-const StackedLabelButton = styled(Button)((props) => ({
-  flexDirection: "column",
-  display: "contents",
-}));
+import { LoremIpsum } from 'lorem-ipsum';
+
+const StackedLabelButton = styled(Button)((/*props*/) => {
+	return {
+		flexDirection: 'column',
+		display: 'contents',
+	};
+});
+
+const drawerWidth = 250;
+
+// eslint-disable-next-line react/prop-types
+function LoremText({ units = 'p', count = '10' }) {
+	const ipsumStuff = new LoremIpsum({
+		units,
+		count,
+		sentencesPerParagraph: {
+			max: 8,
+			min: 4,
+		},
+		wordsPerSentence: {
+			max: 16,
+			min: 4,
+		},
+	});
+	console.log([count, units]);
+	return (
+		<span>
+			{ipsumStuff
+				.generateParagraphs(count)
+				.split(/\n/)
+				.map((e, k) => (
+					<p key={k}>{e}</p>
+				))}
+		</span>
+	);
+}
 
 export default function Demo() {
-  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
-  const [ipsum] = React.useState(<LoremText units={"p"} count={42} />);
+	const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+	const [ipsum] = React.useState(<LoremText units={'p'} count={42} />);
+	const theme = useTheme();
 
-  return (
-    <div>
-      <AppBar position="fixed">
-        <Toolbar variant="dense">
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={() => setIsDrawerOpen(!isDrawerOpen)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6">
-            Shellaced
-            <sup
-              style={{
-                fontSize: "50%",
-              }}
-            >
-              v0.0
-            </sup>
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Toolbar />
-      <Drawer
-        open={isDrawerOpen}
-        onClose={() => setIsDrawerOpen(false)}
-        anchor="left"
-        variant="persistent"
-        sx={{ position: "relative", "z-index": 1 }}
-      >
-        <Toolbar variant="dense" disableGutters={true} />
-        <List>
-          <ListItem>
-            <Stack
-              minWidth="100%"
-              spacing={0.25}
-              alignItems="center"
-              alignContent="center"
-            >
-              <StackedLabelButton startIcon={<PeopleIcon />}>
-                People
-              </StackedLabelButton>
-            </Stack>
-          </ListItem>
-          <ListItem>
-            <Stack
-              minWidth="100%"
-              spacing={0.25}
-              alignItems="center"
-              alignContent="center"
-            >
-              <StackedLabelButton startIcon={<JobsIcon />}>
-                Jobs
-              </StackedLabelButton>
-            </Stack>
-          </ListItem>
-          <ListItem>
-            <Stack
-              minwWidth="100%"
-              spacing={0.25}
-              alignItems="center"
-              alignContent="center"
-            >
-              <StackedLabelButton startIcon={<WorkflowIcon />}>
-                Submittals
-              </StackedLabelButton>
-            </Stack>
-          </ListItem>
-        </List>
-      </Drawer>
-      <Box
-        sx={{
-          p: 1,
-          marginLeft: isDrawerOpen ? 18 : 0,
-          marginBottom: 4,
-        }}
-      >
-        <Typography variant="body2">{ipsum}</Typography>
-      </Box>
-      <Container
-        sx={{
-          position: "fixed",
-          m: 0,
-          bottom: 0,
-          bgcolor: "background.paper",
-        }}
-        style={{ padding: 0 }}
-      >
-        <Divider />
-        <Typography variant="body2" textAlign="center">
-          footer &copy; 2023
-        </Typography>
-      </Container>
-    </div>
-  );
+	return (
+		<div>
+			<AppBar position="fixed">
+				<Toolbar variant="dense">
+					<IconButton
+						edge="start"
+						color="inherit"
+						aria-label="menu"
+						sx={{ mr: 2 }}
+						onClick={() => setIsDrawerOpen(!isDrawerOpen)}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant="h6">
+						Shellaced
+						<sup
+							style={{
+								fontSize: '50%',
+							}}
+						>
+							v0.0
+						</sup>
+					</Typography>
+				</Toolbar>
+			</AppBar>
+			<Toolbar />
+			<Drawer
+				open={isDrawerOpen}
+				onClose={() => setIsDrawerOpen(false)}
+				anchor="left"
+				variant="persistent"
+				sx={{
+					position: 'relative',
+					zIndex: 1,
+					'& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+				}}
+			>
+				<Toolbar variant="dense" disableGutters={true} />
+				<List>
+					<ListItem>
+						<Stack
+							minWidth="100%"
+							spacing={0.25}
+							alignItems="center"
+							alignContent="center"
+						>
+							<StackedLabelButton
+								startIcon={<PeopleIcon />}
+								onClick={() => setIsDrawerOpen(!isDrawerOpen)}
+							>
+								People
+							</StackedLabelButton>
+						</Stack>
+					</ListItem>
+					<ListItem>
+						<Stack
+							minWidth="100%"
+							spacing={0.25}
+							alignItems="center"
+							alignContent="center"
+						>
+							<StackedLabelButton startIcon={<JobsIcon />}>
+								Jobs
+							</StackedLabelButton>
+						</Stack>
+					</ListItem>
+					<ListItem>
+						<Stack
+							minWidth="100%"
+							spacing={0.25}
+							alignItems="center"
+							alignContent="center"
+						>
+							<StackedLabelButton startIcon={<WorkflowIcon />}>
+								Submittals
+							</StackedLabelButton>
+						</Stack>
+					</ListItem>
+				</List>
+			</Drawer>
+			<Box
+				sx={{
+					p: 1,
+					marginLeft: isDrawerOpen ? 35 : 0,
+					marginBottom: 4,
+				}}
+			>
+				<Typography variant="body2" component={'div'}>
+					{ipsum}
+				</Typography>
+			</Box>
+			<Container
+				sx={{
+					position: 'fixed',
+					m: 0,
+					bottom: 0,
+					bgcolor: 'background.paper',
+				}}
+				style={{ padding: 0 }}
+				maxWidth="100%"
+			>
+				<Divider />
+				<Typography variant="body2" textAlign="center" component="div">
+					footer &copy; 2023
+				</Typography>
+			</Container>
+		</div>
+	);
 }
+
